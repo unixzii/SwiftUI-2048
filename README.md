7761b72ceafb04c51fb9b174a061bafda4373842
Cyandev <unixzii@gmail.com>
2020-07-18 01:48:39 +0800

    ✈️  Migrate to SwiftUI 2.0

--- README.md
+++ README.md
@@ -9,13 +9,8 @@ This is a simple game to demonstrate the new SwiftUI framework.
 ## Supported Platforms
 
 * iOS 13.0+
-* macOS 10.15+ (Available in 2 versions)
-
-## Notes about "UIKit for Mac"
-
-**("UIKit for Mac" version is deprecated, and the AppKit version is recommanded)**
-
-This demo has adopted "UIKit for Mac" and uses a dynamic-loaded bundle to enable the full macOS abilities (like the accessing to AppKit, which is used in the demo to handle keyboard events). Specially, this is done by just using Swift. To learn more about it, take a look at the source code of `AppKitSupports`, there is a lot of detailed comments.  
+* macOS 10.15+
+* macOS 11+ (macCatalyst version) 
 
 ## License
 
--- SwiftUI2048/Views/GameView.swift
+++ SwiftUI2048/Views/GameView.swift
@@ -32,23 +32,29 @@ struct GameView : View {
     
     fileprivate struct LayoutTraits {
         let bannerOffset: CGSize
+        let showsBanner: Bool
         let containerAlignment: Alignment
     }
     
     fileprivate func layoutTraits(`for` proxy: GeometryProxy) -> LayoutTraits {
+#if os(macOS)
+        let landscape = false
+#else
         let landscape = proxy.size.width > proxy.size.height
+#endif
         
         return LayoutTraits(
             bannerOffset: landscape
-                ? .init(width: proxy.safeAreaInsets.leading + 32, height: 0)
-                : .init(width: 0, height: proxy.safeAreaInsets.top + 32),
+                ? .init(width: 32, height: 0)
+                : .init(width: 0, height: 32),
+            showsBanner: landscape ? proxy.size.width > 720 : proxy.size.height > 550,
             containerAlignment: landscape ? .leading : .top
         )
     }
     
     var gestureEnabled: Bool {
         // Existed for future usage.
-#if !os(iOS)
+#if os(macOS) || targetEnvironment(macCatalyst)
         return false
 #else
         return true
@@ -66,7 +72,7 @@ struct GameView : View {
                     return
                 }
                 
-                withTransaction(Transaction()) {
+                withTransaction(Transaction(animation: .spring())) {
                     self.ignoreGesture = true
                     
                     if v.translation.width > threshold {
@@ -94,10 +100,12 @@ struct GameView : View {
         GeometryReader { proxy in
             bind(self.layoutTraits(for: proxy)) { layoutTraits in
                 ZStack(alignment: layoutTraits.containerAlignment) {
-                    Text("2048")
-                        .font(Font.system(size: 48).weight(.black))
-                        .color(Color(red:0.47, green:0.43, blue:0.40, opacity:1.00))
-                        .offset(layoutTraits.bannerOffset)
+                    if layoutTraits.showsBanner {
+                        Text("2048")
+                            .font(Font.system(size: 48).weight(.black))
+                            .foregroundColor(Color(red:0.47, green:0.43, blue:0.40, opacity:1.00))
+                            .offset(layoutTraits.bannerOffset)
+                    }
                     
                     ZStack(alignment: .center) {
                         BlockGridView(matrix: self.gameLogic.blockMatrix,
@@ -107,11 +115,12 @@ struct GameView : View {
                 }
                 .frame(width: proxy.size.width, height: proxy.size.height, alignment: .center)
                 .background(
-                    Rectangle().fill(Color(red:0.96, green:0.94, blue:0.90, opacity:1.00))
+                    Rectangle()
+                        .fill(Color(red:0.96, green:0.94, blue:0.90, opacity:1.00))
+                        .edgesIgnoringSafeArea(.all)
                 )
             }
         }
-        .edgesIgnoringSafeArea(.all)
     }
     
     var body: AnyView {
